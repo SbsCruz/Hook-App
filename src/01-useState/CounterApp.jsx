9afa6d93ebec521530a2e334550c5cbfa7c9d23e
Sebasss <sebas.cruz750@gmail.com>
2023-10-31 18:58:44 -0500

    inital commit

---
+++ src/01-useState/CounterApp.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react"
+
+export const CounterApp = () => {
+
+    const [counter, setCounter] = useState({
+        counter1: 10,
+        counter2: 20,
+        counter3: 30,
+
+    });
+
+    const {counter1, counter2, counter3} = counter;
+
+return (
+<>
+    <h1>COUNTER: {counter1}</h1>
+    <h1>COUNTER: {counter2}</h1>
+    <h1>COUNTER: {counter3}</h1>
+    <button onClick={()=>setCounter({
+        ...counter,
+        counter1: counter1+1,
+    })}>+1</button>
+    {console.log(counter1)}
+</>
+)
+}
